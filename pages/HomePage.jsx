25d96ae584ca07f8bdbad4e0699e4b6ac3e641a7
George <ghern5234@gmail.com>
2024-07-18 22:53:33 +0100

    added utils, pages, and  components folders

---
+++ pages/HomePage.jsx
@@ -0,0 +1,6 @@
+// Importing the useState and useEffect hooks from the 'react' library.
+// These hooks are used to add state management and side effects to functional components.
+import { useState, useEffect } from 'react';
+
+// 'Link' is used to create navigational links in a React application.
+import { Link } from 'react-router-dom';
